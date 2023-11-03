fcc2a6aad490a226f29ee5c22cf4f6da5fa08e62
invalid-jess <jessgaming55@gmail.com>
2023-11-03 15:55:49 +0800

    created window using glfw

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use glfw::{fail_on_errors, Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(600, 400, "heat rendering", glfw::WindowMode::Windowed)
+        .expect("failed to create window");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => (),
+            }
+        }
+    }
 }
