46c32f981748cbee2bb52e4ee70f551e5ad3f39a
invalid-jess <jessgaming55@gmail.com>
2023-11-09 21:02:43 +0800

    switched to go. can now display triangle

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/gl/v3.3-compatibility/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+var vertices = []float32{
+	-0.5, 0.0, 0.0,
+	-0.5, 0.5, 0.0,
+	0.5, 0.0, 0.0,
+}
+
+const (
+	vShaderSrc = `#version 330
+
+layout (location = 0) in vec3 position;
+
+void main {
+	gl_Position = vec4(position.x, position.y, position.z , 1.0);
+}` + "\x00"
+	fShaderSrc = `#version 330
+
+layout (location = 0) out vec4 color;
+
+void main {
+	color = vec4(0.0, 0.0, 1.0, 1.0);
+}` + "\x00"
+)
+
+func compileShader(shaderSrc string, shaderType uint32) uint32 {
+	shader := gl.CreateShader(shaderType)
+	cstring, free := gl.Strs(shaderSrc)
+	gl.ShaderSource(shader, 1, cstring, nil)
+	free()
+	gl.CompileShader(shader)
+
+	return shader
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+	window, err := glfw.CreateWindow(600, 400, "heat rendering engine", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	vShader := compileShader(vShaderSrc, gl.VERTEX_SHADER)
+	fShader := compileShader(fShaderSrc, gl.FRAGMENT_SHADER)
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vShader)
+	gl.AttachShader(program, fShader)
+	gl.LinkProgram(program)
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+	gl.VertexAttribPointerWithOffset(0, 3, gl.FLOAT, false, 4*3, uintptr(0))
+	gl.EnableVertexAttribArray(0)
+
+	for !window.ShouldClose() {
+		if window.GetKey(glfw.KeyEscape) == glfw.Press {
+			window.SetShouldClose(true)
+		}
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		gl.UseProgram(program)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
